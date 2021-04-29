d10b0e3d7b9c864acfb76fc524475194515db224
nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <83186068+nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2021-04-28 20:05:29 -0800

    Create ok.cs

---
+++ ok.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ok = "ok";
+            Console.Title = ok;
+            Console.WriteLine(ok);
+            Console.ReadKey();
+        }
+    }
+}
